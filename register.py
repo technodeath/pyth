4793e3ef364e7ea2419ca768bd8ac536d239cb10
Alex Kuzmitski <akuzmitski@scnsoft.com>
2021-03-23 17:09:05 +0300

    added additional classes

---
+++ register.py
@@ -0,0 +1,3 @@
+class Register:
+
+    def __init__(self):
--- test_login.py
+++ test_login.py
@@ -16,19 +16,36 @@ class TestLogin(unittest.TestCase):
     def test_login(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="technodeath@gmail.com", password="qwe123")
+        self.find_item_on_site(wd, item="Blouse")
+        self.click_on_item(wd, item="Blouse")
+        self.click_add_to_cart(wd)
+        self.click_close_popup(wd)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Sign out").click()
 
-    def login(self, wd):
+    def find_item_on_site(self, wd, item):
+        wd.find_element_by_id("search_query_top").send_keys(item)
+        wd.find_element_by_css_selector("button[name='submit_search']").click()
+
+    def click_on_item(self, wd, item):
+        wd.find_element_by_css_selector("a[title='%s']" % item).click()
+
+    def click_add_to_cart(self, wd):
+        wd.find_element_by_id("add_to_cart").click()
+
+    def click_close_popup(self, wd):
+        wd.find_element_by_css_selector("span.cross").click()
+
+    def login(self, wd, username, password):
         wd.find_element_by_link_text("Sign in").click()
         wd.find_element_by_id("email").click()
         wd.find_element_by_id("email").clear()
-        wd.find_element_by_id("email").send_keys("technodeath@gmail.com")
+        wd.find_element_by_id("email").send_keys(username)
         wd.find_element_by_id("passwd").clear()
-        wd.find_element_by_id("passwd").send_keys("qwe123")
+        wd.find_element_by_id("passwd").send_keys(password)
         wd.find_element_by_xpath("//button[@id='SubmitLogin']/span").click()
 
     def open_home_page(self, wd):
